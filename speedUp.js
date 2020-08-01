bc3ef936ad49ba5eb98307752d1ef85d76fc06bd
peidrao <contatopedrorn@gmail.com>
2020-08-01 09:45:47 -0300

    adicionando botao de velocidade normal

--- speedUp.js
+++ speedUp.js
@@ -1,20 +1,20 @@
 const interval = setInterval(() => {
-  const header = document.querySelector('._1QUKR');
+  const header = document.querySelector('._3All_');
   
   if(header) {
     clearInterval(interval);
     
-    const button = document.createElement("button"); // Velocidade 2x ou mais
     const button2 = document.createElement("button"); // Velocidade normal
+    const button = document.createElement("button"); // Velocidade 2x ou mais
 
-    button.innerHTML = "2.5x";
     button2.innerHTML = "1x"
+    button.innerHTML = "2.5x";
 
-    button.classList.add("increaseSpeed");
-    button2.classList.add("oneTime");
+    button2.classList.add("x1");
+    button.classList.add("x2");
     
-    header.appendChild(button)
     header.appendChild(button2)
+    header.appendChild(button)
     
     // Velocidade 2.5x
     button.addEventListener("click", () => {
@@ -33,7 +33,7 @@ const interval = setInterval(() => {
       });
     })
 
-    header.appendChild(button);
     header.appendChild(button2);
+    header.appendChild(button);
   }
 }, 1000)
